5ffe955a6d77dcf7a6d5b2cfc3490b8a52df75fd
Prakash Tripathi <prakashtripathi@admins-MacBook-Air.local>
2020-10-28 00:21:02 +0530

    Initial comit

---
+++ pages/index.js
@@ -0,0 +1,71 @@
+import fetch from "isomorphic-fetch";
+import Error from "next/error";
+import StoryList from "../components/StoryList";
+import Layout from "../components/Layout";
+import Link from "next/link";
+
+class Index extends React.Component {
+  static async getInitialProps({ req, res, query }) {
+    let stories;
+    let page;
+    try {
+      page = Number(query.page) || 1;
+
+      const response = await fetch(
+        `https://node-hnapi.herokuapp.com/news?page=${page}`
+      );
+      stories = await response.json();
+    } catch (error) {
+      console.log(error);
+      stories = [];
+    }
+    return { page, stories };
+  }
+
+  componentDidMount() {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(registration => {
+          console.log("service worker registration successful", registration);
+        })
+        .catch(err => {
+          console.log("service worker registration failed", err.message);
+        });
+    }
+  }
+
+  render() {
+    const { stories, page } = this.props;
+    if (stories.length === 0) {
+      return <Error statusCode={503} />;
+    }
+    return (
+      <Layout
+        title="Hacker Next"
+        description="A Hacker news clone made with NextJS"
+      >
+        <StoryList stories={stories} />
+        <footer>
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next Page ({page + 1})</a>
+          </Link>
+        </footer>
+        <style jsx>
+          {`
+            footer {
+              padding: 1em;
+            }
+            footer a {
+              font-weight: bold;
+              color: black;
+              text-decoration: none;
+            }
+          `}
+        </style>
+      </Layout>
+    );
+  }
+}
+
+export default Index;
